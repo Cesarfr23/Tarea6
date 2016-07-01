376e256a55a2464924053258649d1bc594f634b0
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-02 00:16:10 +0100

    Codificando la justificacion

--- Tarea6/JustificarCadena.Designer.cs
+++ Tarea6/JustificarCadena.Designer.cs
@@ -29,51 +29,67 @@
         private void InitializeComponent()
         {
             this.CadenaJustificadaLabel = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.JustificarListBox = new System.Windows.Forms.ListBox();
+            this.JustificarTextBox = new System.Windows.Forms.TextBox();
             this.JustificarComboBox = new System.Windows.Forms.ComboBox();
+            this.JustificarButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CadenaJustificadaLabel
             // 
             this.CadenaJustificadaLabel.AutoSize = true;
-            this.CadenaJustificadaLabel.Location = new System.Drawing.Point(26, 26);
+            this.CadenaJustificadaLabel.Location = new System.Drawing.Point(26, 119);
             this.CadenaJustificadaLabel.Name = "CadenaJustificadaLabel";
             this.CadenaJustificadaLabel.Size = new System.Drawing.Size(120, 13);
             this.CadenaJustificadaLabel.TabIndex = 0;
             this.CadenaJustificadaLabel.Text = "Introdusca Una Cadena";
             // 
-            // listBox1
+            // JustificarListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(29, 218);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(508, 95);
-            this.listBox1.TabIndex = 1;
+            this.JustificarListBox.FormattingEnabled = true;
+            this.JustificarListBox.Location = new System.Drawing.Point(29, 218);
+            this.JustificarListBox.Name = "JustificarListBox";
+            this.JustificarListBox.Size = new System.Drawing.Size(508, 95);
+            this.JustificarListBox.TabIndex = 1;
             // 
-            // textBox1
+            // JustificarTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 149);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(508, 20);
-            this.textBox1.TabIndex = 2;
+            this.JustificarTextBox.Location = new System.Drawing.Point(29, 149);
+            this.JustificarTextBox.Name = "JustificarTextBox";
+            this.JustificarTextBox.Size = new System.Drawing.Size(508, 20);
+            this.JustificarTextBox.TabIndex = 2;
             // 
             // JustificarComboBox
             // 
             this.JustificarComboBox.FormattingEnabled = true;
-            this.JustificarComboBox.Location = new System.Drawing.Point(189, 26);
+            this.JustificarComboBox.Items.AddRange(new object[] {
+            "Justificar a la Derecha",
+            "Justificar a la Izquierda"});
+            this.JustificarComboBox.Location = new System.Drawing.Point(152, 56);
             this.JustificarComboBox.Name = "JustificarComboBox";
-            this.JustificarComboBox.Size = new System.Drawing.Size(121, 21);
+            this.JustificarComboBox.Size = new System.Drawing.Size(234, 21);
             this.JustificarComboBox.TabIndex = 3;
+            this.JustificarComboBox.SelectedIndexChanged += new System.EventHandler(this.JustificarComboBox_SelectedIndexChanged);
+            // 
+            // JustificarButton
+            // 
+            this.JustificarButton.Location = new System.Drawing.Point(410, 54);
+            this.JustificarButton.Name = "JustificarButton";
+            this.JustificarButton.Size = new System.Drawing.Size(75, 23);
+            this.JustificarButton.TabIndex = 4;
+            this.JustificarButton.Text = "Justificar";
+            this.JustificarButton.UseVisualStyleBackColor = true;
+            this.JustificarButton.Click += new System.EventHandler(this.JustificarButton_Click);
             // 
             // JustificarCadena
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 429);
+            this.Controls.Add(this.JustificarButton);
             this.Controls.Add(this.JustificarComboBox);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.JustificarTextBox);
+            this.Controls.Add(this.JustificarListBox);
             this.Controls.Add(this.CadenaJustificadaLabel);
             this.MinimumSize = new System.Drawing.Size(283, 364);
             this.Name = "JustificarCadena";
@@ -86,8 +102,9 @@
         #endregion
 
         private System.Windows.Forms.Label CadenaJustificadaLabel;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox JustificarListBox;
+        private System.Windows.Forms.TextBox JustificarTextBox;
         private System.Windows.Forms.ComboBox JustificarComboBox;
+        private System.Windows.Forms.Button JustificarButton;
     }
 }
--- Tarea6/JustificarCadena.cs
+++ Tarea6/JustificarCadena.cs
@@ -16,5 +16,31 @@ namespace Tarea6
         {
             InitializeComponent();
         }
+        private void JustificarComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void JustificarButton_Click(object sender, EventArgs e)
+        {
+
+            string cadena = Convert.ToString(JustificarTextBox.Text);
+            string resultado = JustificarComboBox.Text;
+
+            switch(resultado)
+            {
+                case "Justificar a la Derecha":
+                    resultado = cadena.PadLeft(5, ' ');
+                    JustificarListBox.Items.Add(resultado);
+                    break;
+                case "Justificar a la Izquierda":
+                    resultado = cadena.PadRight(5, ' ');
+                    JustificarListBox.Items.Add(resultado);
+                    break;
+                default:
+                    MessageBox.Show("Elija Una Opcion");
+                    break;
+            }
+        }
     }
 }
